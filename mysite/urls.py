ee793840d5f840960f453f6d3490a3cddd4649e5
gayachi <pjh72bell@gmail.com>
2019-02-26 21:04:46 +0900

    renew gagjoo

--- mysite/urls.py
+++ mysite/urls.py
@@ -13,9 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import include, url
 from django.contrib import admin
-from django.urls import path
+
+from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('polls/',include('polls.urls')),
 ]
+#include plug and play URLconf 혹같은거지 붙였다뗴는
+'''urlpatterns = [
+    url(r'^polls/', include('polls.urls')),
+    url(r'^admin/', admin.site.urls),
+]'''
--- polls/views.py
+++ polls/views.py
@@ -3,5 +3,6 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("http://cfile205.uf.daum.net/image/15696F344F76928530F475")
 # Create your views here.
+#django view polls
