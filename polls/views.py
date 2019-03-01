b8bb4c32533e0a456d9c7dbbd92178797ab1d744
gayachi <pjh72bell@gmail.com>
2019-03-01 22:35:14 +0900

    renew somethong

--- polls/views.py
+++ polls/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("http://cfile205.uf.daum.net/image/15696F344F76928530F475")
+    return HttpResponse("heloo")
+#HttpResponse only text available
 # Create your views here.
 #django view polls
